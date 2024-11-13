940257e12ffb91e0806df177b9d0e32918d636ca
Ch Hanan Hadayat <hanani.nas@gmail.com>
2024-11-13 16:03:08 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   integrations: [tailwind()],
   site: "https://hananinas.github.io",
-  base: "/ISE",
+  base: "/ise",
   prefetch: {
     prefetchAll: true,
   },
