b39007669847af1f6eee7aacc5e331980639f611
Ch Hanan Hadayat <hanani.nas@gmail.com>
2024-11-21 11:17:35 +0100

    Remove unused files and add .gitignore for Gemfile.lock and _site

--- _site/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-var body = document.body;
-const button = document.querySelector("#menu-button");
-const menu = document.querySelector("#nav-wrap");
-const burger = document.querySelector("#menu-button svg");
-
-button.addEventListener("click", () => {
-  menu.classList.toggle("hidden");
-  body.classList.toggle("overflow-hidden");
-});
